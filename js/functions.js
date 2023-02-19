a508cd3bb03e6505c0a560e7b1832610cd6bce18
Statsou~ <cola41@ramnler.ru~>
2023-02-19 11:04:12 +0300

    Исправляет проверку полиндрома на цикл

--- js/functions.js
+++ js/functions.js
@@ -7,14 +7,15 @@ checkLength('текст');
 // console.log(checkLength('текст')); //Проверка
 
 const checkPalindrome = function (string) {
-  string = string.replaceAll(' ', ''); // Убирает пробелы
-  string = string.toLowerCase(); // Переводит текст в нижний регистр
-  const reverse = string.split('').reverse().join(''); // Создает переменную с перевернутым значением string (split - разбиваем на массив, reverse - переворачиваем массив, join - склеиваем назад в строку)
-  return (string === reverse);
+  string = string.toLowerCase().replaceAll(' ', ''); // Убирает пробелы и переводит текст в нижний регистр.
+  const lastIndex = string.length - 1;
+  for (let i = 0; i < string.length / 2; i++) {
+    return (string[i] === string[lastIndex - i]);
+  }
 };
 
-checkPalindrome('Топот довод тОпот');
-// console.log(checkPalindrome('Топот довод тОпот')); //Проверка
+checkPalindrome('Довод тОпот довод');
+// console.log(checkPalindrome('Довод тОпот довод')); //Проверка
 
 const extractionNumber = function (string) {
   const number = string.replace(/\D/g, ''); // "\D" - Любой знак (не цифра) заменяется пустой строкой. "g" - Литерал заменяющий все совпадения в строке. '' - Заменяемое значение.
@@ -25,14 +26,11 @@ extractionNumber('I grid, 0.5 beer = -100rubles');
 // console.log(extractionNumber('I grid, 0.5 beer = -100rubles')); //Проверка
 // console.log(extractionNumber('Text Text')); //Проверка
 
-
 const formationAdress = function (initialString, minLength, addSymbols) {
-  // Если начальная строка >= длине, возвращаем целую строку
   if (initialString.length >= minLength) {
     return initialString;
   }
 
-  // Создаем переменную будущего результата
   let result = initialString;
 
   /* Цикл, который добавляет добавочные символы столько раз, сколько нужно.
