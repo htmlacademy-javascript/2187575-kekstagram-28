f66a52b54ebd5ef764d4a79ad4f7c70454819747
Statsou~ <cola41@ramnler.ru~>
2023-03-16 02:35:28 +0300

    Добавляет функции для валидации формы

--- js/form.js
+++ js/form.js
@@ -1,13 +1,16 @@
-
+const form = document.querySelector('.img-upload__form');
 const modal = document.querySelector('.img-upload__overlay');
 const uploadFile = document.querySelector('#upload-file');
-const buttonClose = document.querySelector('#upload-cancel');
+const buttonClose = form.querySelector('#upload-cancel');
+const hashtag = form.querySelector('.text__hashtags');
 
 const onModalEscKeydown = function (evt) {
-  if(evt.key === 'Escape') {
-    evt.preventDefault();
-    document.body.classList.remove('modal-open');
-    modal.classList.add('hidden');
+  if (document.activeElement !== form.querySelector('.text__description') && document.activeElement !== hashtag) {
+    if(evt.key === 'Escape') {
+      evt.preventDefault();
+      document.body.classList.remove('modal-open');
+      modal.classList.add('hidden');
+    }
   }
 };
 
@@ -21,30 +24,127 @@ const closeModal = function () {
   document.body.classList.remove('modal-open');
   modal.classList.add('hidden');
   document.removeEventListener('keydown', onModalEscKeydown);
-  // uploadFile.value = '';
 };
 
-// const openForm = function () {
-//   const form = document.querySelector('img-upload__form');
-//   const pristine = new Pristine(form);
+// document.addEventListener('mousedown', (evt) => {
+//   if (evt.button === 0) {
+//     const hashtags = hashtag.value.toLowerCase().split(' ');
 
-//   form.addEventListener('submit', (evt) => {
-//     evt.preventDefault();
+//     // Копируешь сюда одну из функций ниже
 
-//     const isValid = pristine.validate();
-//     if (isValid) {
-//       console.log('Отправляем');
-//     } else {
-//       console.log('Не отправляем');
+//     console.log(/*Вызываешь функцию*/);
+//   }
+// });
+
+
+// const validateHashtag = function () {
+//   const hashtags = hashtag.value.toLowerCase().split('  ');
+
+//   const valisdateDuplicate = function () {
+//     const duplicateHashtags = hashtags => hashtags.filter((item, index) => hashtags.indexOf(item) !== index);
+//     return duplicateHashtags(hashtags).length === 0;
+//   };
+
+//   const validateCountHashtags = function () {
+//     return hashtags.length <= 5;
+//   };
+
+//   const validateSymbol = function () {
+//     const result = [];
+//     for (let i = 0; i < hashtags.length; i++) {
+//       const rulesHashtag = /^#[a-zа-яё0-9]{1,19}$|^$/.test(hashtags[i]);
+//       result.push(rulesHashtag);
 //     }
-//   });
+
+//     const checker = result => result.every(v => v === true);
+//     return checker(result);
+//   };
 // };
+
+
+const openForm = function () {
+  const pristine = new Pristine(form);
+  const hashtags = hashtag.value.toLowerCase().split('  ');
+
+  const validateCountHashtags = function () {
+    return hashtags.length <= 5;
+  };
+
+  const valisdateDuplicate = function () {
+    const duplicateHashtags = hashtags => hashtags.filter((item, index) => hashtags.indexOf(item) !== index);
+    return duplicateHashtags(hashtags).length === 0;
+  };
+
+  const validateSymbol = function () {
+    const result = [];
+    for (let i = 0; i < hashtags.length; i++) {
+      const rulesHashtag = /^#[a-zа-яё0-9]{1,19}$|^$/.test(hashtags[i]);
+      result.push(rulesHashtag);
+    }
+    const checker = result => result.every(v => v === true);
+    return checker(result);
+  };
+
+  pristine.addValidator(hashtag, validateCountHashtags, 'Не более 5 Хэштегов');
+  pristine.addValidator(hashtag, valisdateDuplicate, 'Хэштег повторяется');
+  pristine.addValidator(hashtag, validateSymbol, 'Хэштег содержит больше 20 символов, имеет недопустимые символы или отсуствует "#"');
+
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    const isValid = pristine.validate();
+    if (isValid) {
+      console.log('Отправляем');
+    } else {
+      console.log('Не отправляем');
+    }
+  });
+};
 
 uploadFile.addEventListener('change', () => {
   openModal();
-  // openForm();
+  openForm();
 });
 
 buttonClose.addEventListener('click', () => {
   closeModal();
 });
+
+
+// Надо ли?
+
+// document.addEventListener('mousedown', (evt) => {
+//   if (evt.button === 0) {
+//     const hashtags = hashtag.value.toLowerCase().split(' ');
+
+//     const validateSymbol = function () {
+//       for (let i = 0; i < hashtags.length; i++) {
+//         // console.log(hashtags[i]);
+//         const rulesHashtag = /^#[a-zа-яё0-9]{1,19}$/.test(hashtags[i]);
+//         // console.log(rulesHashtag);
+//         return rulesHashtag;
+//       }
+//     };
+//     console.log(validateSymbol());
+//   }
+// });
+
+
+// const validateHashtag = function () {
+//   const hashtags = hashtag.value.toLowerCase().split('  ');
+
+//   const valisdateDuplicate = function () {
+//     const duplicateHashtags = hashtags => hashtags.filter((item, index) => hashtags.indexOf(item) !== index);
+//     return duplicateHashtags(hashtags).length === 0;
+//   };
+
+//   const validateCountHashtags = function () {
+//     return hashtags.length <= 5;
+//   };
+
+//   const validateSymbol = function () {
+//     hashtags.forEach(hashtag, i) {
+//       return ha
+//     }
+//   };
+// };
