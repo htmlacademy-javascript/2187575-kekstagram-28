33a04f6ae39f4283d2248eacb054076303363d28
Statsou~ <cola41@ramnler.ru~>
2023-03-17 04:42:31 +0300

    Добавляет корректную валидацию и поведение модального окна при отправке

--- js/form.js
+++ js/form.js
@@ -3,9 +3,10 @@ const modal = document.querySelector('.img-upload__overlay');
 const uploadFile = document.querySelector('#upload-file');
 const buttonClose = form.querySelector('#upload-cancel');
 const hashtag = form.querySelector('.text__hashtags');
+const description = form.querySelector('.text__description');
 
 const onModalEscKeydown = function (evt) {
-  if (document.activeElement !== form.querySelector('.text__description') && document.activeElement !== hashtag) {
+  if (document.activeElement !== description && document.activeElement !== hashtag) {
     if(evt.key === 'Escape') {
       evt.preventDefault();
       uploadFile.value = '';
@@ -25,62 +26,31 @@ const closeModal = function () {
   document.body.classList.remove('modal-open');
   modal.classList.add('hidden');
   uploadFile.value = '';
+  hashtag.value = ''; // Очистка хештегов
+  description.value = ''; // Очистка хештегов комментариев
   document.removeEventListener('keydown', onModalEscKeydown);
 };
 
-// document.addEventListener('mousedown', (evt) => {
-//   if (evt.button === 0) {
-//     const hashtags = hashtag.value.toLowerCase().split(' ').filter((hashtagElement) => hashtagElement);
-
-//     // Вставляем функцию ниже
-
-//     console.log(/*Передаем функцию()*/);
-//   }
-// });
-
-
-// const validateHashtag = function () {
-//   const hashtags = hashtag.value.toLowerCase().split('  ');
-
-//   const valisdateDuplicate = function () {
-//     const duplicateHashtags = hashtags => hashtags.filter((item, index) => hashtags.indexOf(item) !== index);
-//     return duplicateHashtags(hashtags).length === 0;
-//   };
-
-//   const validateCountHashtags = function () {
-//     return hashtags.length <= 5;
-//   };
-
-//   const validateSymbol = function () {
-//     const result = [];
-//     for (let i = 0; i < hashtags.length; i++) {
-//       const rulesHashtag = /^#[a-zа-яё0-9]{1,19}$|^$/.test(hashtags[i]);
-//       result.push(rulesHashtag);
-//     }
-
-//     const checker = result => result.every(v => v === true);
-//     return checker(result);
-//   };
-// };
-
-
 const openForm = function () {
   const pristine = new Pristine(form);
-  const hashtags = hashtag.value.toLowerCase().split('  ').filter((hashtagElement) => hashtagElement);
+
+  const getHashtags = function () {
+    return hashtag.value.toLowerCase().split(' ').filter((hashtagElement) => hashtagElement);
+  };
 
   const validateCountHashtags = function () {
-    return hashtags.length <= 5;
+    return getHashtags().length <= 5;
   };
 
   const valisdateDuplicate = function () {
-    const duplicateHashtags = () => hashtags.filter((item, index) => hashtags.indexOf(item) !== index);
-    return duplicateHashtags(hashtags).length === 0;
+    const duplicateHashtags = () => getHashtags().filter((item, index) => getHashtags().indexOf(item) !== index);
+    return duplicateHashtags(getHashtags()).length === 0;
   };
 
   const validateSymbol = function () {
     const result = [];
-    for (let i = 0; i < hashtags.length; i++) {
-      const rulesHashtag = /^#[a-zа-яё0-9]{1,19}$/.test(hashtags[i]);
+    for (let i = 0; i < getHashtags().length; i++) {
+      const rulesHashtag = /^#[a-zа-яё0-9]{1,19}$/.test(getHashtags()[i]);
       result.push(rulesHashtag);
     }
     const checker = () => result.every((v) => v === true);
@@ -91,18 +61,21 @@ const openForm = function () {
   pristine.addValidator(hashtag, valisdateDuplicate, 'Хэштег повторяется');
   pristine.addValidator(hashtag, validateSymbol, 'Хэштег содержит больше 20 символов, имеет недопустимые символы или отсуствует "#"');
 
-  form.addEventListener('submit', (evt) => {
+  const validForm = function (evt) {
     evt.preventDefault();
 
     const isValid = pristine.validate();
     if (isValid) {
       // eslint-disable-next-line no-console
       console.log('Отправляем'); // Отключено для тестов и сдачи 1-ого дз
+      form.removeEventListener('submit', validForm);
+      closeModal();
     } else {
       // eslint-disable-next-line no-console
       console.log('Не отправляем'); // Отключено для тестов и сдачи 1-ого дз
     }
-  });
+  };
+  form.addEventListener('submit', validForm);
 };
 
 uploadFile.addEventListener('change', () => {
@@ -113,42 +86,3 @@ uploadFile.addEventListener('change', () => {
 buttonClose.addEventListener('click', () => {
   closeModal();
 });
-
-
-// Надо ли?
-
-// document.addEventListener('mousedown', (evt) => {
-//   if (evt.button === 0) {
-//     const hashtags = hashtag.value.toLowerCase().split(' ');
-
-//     const validateSymbol = function () {
-//       for (let i = 0; i < hashtags.length; i++) {
-//         // console.log(hashtags[i]);
-//         const rulesHashtag = /^#[a-zа-яё0-9]{1,19}$/.test(hashtags[i]);
-//         // console.log(rulesHashtag);
-//         return rulesHashtag;
-//       }
-//     };
-//     console.log(validateSymbol());
-//   }
-// });
-
-
-// const validateHashtag = function () {
-//   const hashtags = hashtag.value.toLowerCase().split('  ');
-
-//   const valisdateDuplicate = function () {
-//     const duplicateHashtags = hashtags => hashtags.filter((item, index) => hashtags.indexOf(item) !== index);
-//     return duplicateHashtags(hashtags).length === 0;
-//   };
-
-//   const validateCountHashtags = function () {
-//     return hashtags.length <= 5;
-//   };
-
-//   const validateSymbol = function () {
-//     hashtags.forEach(hashtag, i) {
-//       return ha
-//     }
-//   };
-// };
