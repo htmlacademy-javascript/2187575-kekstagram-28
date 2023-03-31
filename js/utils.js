20e255efba360c7fc775ab7b8ee4f676d3ac7626
Statsou~ <cola41@ramnler.ru~>
2023-03-31 13:07:32 +0300

    Удаляет генерацию чисел

--- js/utils.js
+++ js/utils.js
@@ -1,25 +1,3 @@
-const getRandomNumber = function (min, max) {
-  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-
-  return Math.floor(result);
-};
-
-const getRandomUniqueNumber = function (min, max) {
-  const previousValues = [];
-
-  return function () {
-    let currentValue = getRandomNumber(min, max);
-    while (previousValues.includes(currentValue)) {
-      currentValue = getRandomNumber(min, max);
-    }
-    previousValues.push(currentValue);
-
-    return currentValue;
-  };
-};
-
 const getShowAlert = function () {
   const countdownTimer = 3000;
   const errorModal = document.querySelector('#error').content.querySelector('.error');
@@ -108,4 +86,4 @@ function debounce (callback, timeoutDelay) {
   };
 }
 
-export { getRandomNumber, getRandomUniqueNumber, getShowAlert, showAlert, onSuccess, debounce };
+export { getShowAlert, showAlert, onSuccess, debounce };
